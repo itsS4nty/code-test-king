90e29ac7c39b51c64149fb2a74e378a18f445fc7
itsS4nty <50720295+itsS4nty@users.noreply.github.com>
2023-04-11 18:38:47 +0200

    Add readme and init npm

---
+++ README.md
@@ -0,0 +1,41 @@
+# TypeScript Project
+
+This project uses TypeScript to perform operations on nested objects.
+
+## Requirements
+
+- Node.js
+- npm
+
+## Installation
+
+To install the project dependencies, run the following command:
+
+```
+npm install
+```
+
+## Compilation
+To compile the TypeScript code, run the following command:
+
+```
+npm run build
+```
+
+This will generate a JavaScript file in the dist folder.
+
+## Running the project
+To run the compiled JavaScript file, use the following command:
+
+```
+npm start
+```
+
+## Building and running the project in one command
+To build and run the project in a single command, use the following command:
+
+```
+npm run start:build
+```
+
+This will compile the TypeScript code and run the resulting JavaScript file.
--- dist/codetest.js
+++ dist/codetest.js
@@ -1,18 +1,11 @@
-type StringObject = { [key: string]: string };
-
-export type UnpredictableObject = {
-  a: number;
-  c: any;
-  b?: StringObject;
-};
-
-const stringObject: StringObject = {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const stringObject = {
     a: "343",
     c: "444",
     d: "344",
 };
-
-const unpredictable: UnpredictableObject = {
+const unpredictable = {
     a: 22,
     c: {
         xcd: {
@@ -33,28 +26,23 @@ const unpredictable: UnpredictableObject = {
         },
     },
 };
-
 // Function to find the first 'xyz' array containing the checkObject within the unpredictable object
-const resolveXYZArray = (checkObject: StringObject, unpredictable: UnpredictableObject): Array<StringObject> | null => {
+const resolveXYZArray = (checkObject, unpredictable) => {
     // Check if the checkObject and unpredictable object are not null or undefined
-    if (!checkObject || !unpredictable) return null;
-
+    if (!checkObject || !unpredictable)
+        return null;
     // Call findAllKeys to get all occurrences of the 'xyz' key within the unpredictable object
     let keys_values = findAllKeys(unpredictable);
-
     // Find the first 'xyz' array that contains the checkObject by comparing their JSON string representations
     return keys_values.find(x => JSON.stringify(checkObject) === JSON.stringify(x));
-
     // If you want to return all 'xyz' arrays containing the checkObject, use the filter method instead:
     // return keys.filter(x => JSON.stringify(checkObject) === JSON.stringify(x));
-}
-
+};
 // Function to find all occurrences of the targetKey in the unpredictable object.
 // By default, the targetKey is 'xyz', but it can be changed to search for other keys.
-const findAllKeys = (obj: UnpredictableObject, targetKey: string = 'xyz'): Array<any> => {
+const findAllKeys = (obj, targetKey = 'xyz') => {
     // Initialize an array to store the results
-    let results: Array<any> = [];
-
+    let results = [];
     // Iterate over the keys in the object
     for (const key in obj) {
         // If the current key matches the targetKey and its value is an array, add it to the results
@@ -62,13 +50,13 @@ const findAllKeys = (obj: UnpredictableObject, targetKey: string = 'xyz'): Array
             results.push(obj[key]);
         // If the current value is an object (and not an array), recursively search for the targetKey in the nested object
         else if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
-            const nestedResults = findAllKeys(obj[key] as UnpredictableObject, targetKey);
+            const nestedResults = findAllKeys(obj[key], targetKey);
             // Concatenate the nested results with the main results array
             results = results.concat(nestedResults);
-        } 
+        }
         // If the current value is an array, iterate over its elements
         else if (Array.isArray(obj[key])) {
-            const objKeyArray: Array<UnpredictableObject> = obj[key] as Array<UnpredictableObject>;
+            const objKeyArray = obj[key];
             for (const item of objKeyArray) {
                 // If the current element is an object, recursively search for the targetKey in the nested object
                 if (typeof item === "object") {
@@ -79,11 +67,9 @@ const findAllKeys = (obj: UnpredictableObject, targetKey: string = 'xyz'): Array
             }
         }
     }
-
     // Flatten the results array and return it
     return results.flat();
 };
-
 // Call the resolveXYZArray function with the stringObject and unpredictable object
 let result = resolveXYZArray(stringObject, unpredictable);
-console.log(result)
+console.log(result);
